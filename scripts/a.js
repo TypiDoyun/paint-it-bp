93add94cb50e1e88e38da7208dd43f1c982e5d3d
typidoyun <typidoyun@gmail.com>
2024-08-29 18:10:45 +0900

    chore: Update .gitignore to include JavaScript files

--- scripts/a.js
+++
@@ -1,7 +0,0 @@
-import { Vector3 } from "./utils/math/vector";
-const sdBox = (p, b) => {
-    const q = p.clone.abs().sub(b);
-    return (q.clone.max(Vector3.zero).length +
-        Math.min(Math.max(q.x, Math.max(q.y, q.z)), 0.0));
-};
-console.log(sdBox(new Vector3(-2, 0, -1), new Vector3(1, 1, 1)));
