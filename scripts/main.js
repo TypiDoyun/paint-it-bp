64888e306c4d15dcfc32d03e7f5f2b6a51af58b0
typidoyun <typidoyun@gmail.com>
2024-08-29 18:18:28 +0900

    feat: Register custom components for sketcher, painter, and eraser
    This commit registers custom components for the sketcher, painter, and eraser in both TypeScript and JavaScript files. It adds the necessary code to register these components with the item component registry during the world initialization. These changes enhance the functionality of the codebase by allowing the usage of these custom components in the game.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
 import { world } from "@minecraft/server";
 import { commandHandler } from "./handlers/command";
+import { SketcherSettingComponent } from "./components/sketcher-setting";
+import { EraserComponent } from "./components/eraser";
+import { PainterComponent } from "./components/painter";
+import { SketcherComponent } from "./components/sketcher";
+import { EraserSettingComponent } from "./components/eraser-setting";
+import { PainterSettingComponent } from "./components/painter-setting";
+// Register the command handler
 world.beforeEvents.chatSend.subscribe(commandHandler);
+// Register the custom components
+world.beforeEvents.worldInitialize.subscribe(eventData => {
+    // Register the item custom components
+    eventData.itemComponentRegistry.registerCustomComponent("typidoyun:sketcher", new SketcherComponent());
+    eventData.itemComponentRegistry.registerCustomComponent("typidoyun:painter", new PainterComponent());
+    eventData.itemComponentRegistry.registerCustomComponent("typidoyun:eraser", new EraserComponent());
+    eventData.itemComponentRegistry.registerCustomComponent("typidoyun:sketcher_setting", new SketcherSettingComponent());
+    eventData.itemComponentRegistry.registerCustomComponent("typidoyun:painter_setting", new PainterSettingComponent());
+    eventData.itemComponentRegistry.registerCustomComponent("typidoyun:eraser_setting", new EraserSettingComponent());
+});
